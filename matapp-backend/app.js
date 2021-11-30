e067613698c4ea337da552a771bced5ed260cd87
ematthiasen <89933203+ematthiasen@users.noreply.github.com>
2021-11-30 19:55:33 +0100

    Implemented token/user extraction middleware

--- matapp-backend/app.js
+++ matapp-backend/app.js
@@ -27,6 +27,7 @@ mongoose.connect(config.MONGODB_URI)
 app.use(cors())
 app.use(express.json())
 app.use(middleware.requestLogger)
+app.use(middleware.userExtractor)
 
 //app.use(express.static('build'))
 app.use('/static', express.static(path.join(__dirname, 'build//static')))
--- matapp-backend/utils/middleware.js
+++ matapp-backend/utils/middleware.js
@@ -1,4 +1,29 @@
 const logger = require('./logger')
+const jwt = require('jsonwebtoken')
+
+const userExtractor = (request, response, next) => {
+  const authorization = request.get('authorization')
+
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    logger.debug('Authorization string found', authorization)
+    const decodedToken = jwt.verify(authorization.substring(7), process.env.SECRET)
+
+    if (!decodedToken.id) {
+      const error = new Error()
+      error.name = 'TokenError'
+      error.message = 'Token invalid'
+      throw error
+    }
+    request.user = {
+      username: decodedToken.username,
+      id: decodedToken.id
+    }
+    logger.debug('token decoded, user added to request: ', request.user)
+  } else {
+    logger.debug('No authorization header, anonymous user')
+  }
+  next()
+}
 
 
 const requestLogger = (request, response, next) => {
@@ -16,6 +41,10 @@ const errorHandler = (error, request, response, next) => {
   logger.error('Error handler middleware: error.message:', error.message)
 
   if (error.name === 'AuthenticationError') {
+    return response.status(401).json({ error: error.message })
+  }
+
+  if (error.name === 'JsonWebTokenError') {
     return response.status(401).json({ error: error.message })
   }
 
@@ -41,5 +70,6 @@ const errorHandler = (error, request, response, next) => {
 module.exports = 
 {
   requestLogger,
-  errorHandler
+  errorHandler,
+  userExtractor
 }
